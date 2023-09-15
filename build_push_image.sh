f2f1e4d11c61521c6aa119b28215a656fe1cb06f
Agastiya <ageztya.putra@gmail.com>
2023-09-15 22:47:40 +0700

    tambah keterangan pas build_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Melakukan build docker image
 docker build . -t item-app:v1
 
+# Menampilkan list images
 docker images
 
+# Mengubah nama image agar sesuai dengan format GitHub Packages
 docker tag item-app:v1 ghcr.io/agastiya/item-app:v1
 
+# melakukan login ke GitHub Packages dengan password yang sudah diset sebelumnya (via terminal)
 echo $PASSWORD_DOCKER_HUB | docker login --username agastiya --password-stdin ghcr.io
 
+# melakukan push image ke GitHub Packages
 docker push ghcr.io/agastiya/item-app:v1
